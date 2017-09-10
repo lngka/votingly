f6500a88f167e08d52ff5a6058e44621ba064d3f
lngka <foxlead@gmail.com>
2017-09-10 14:42:44 +0200

    chart tooltips to display percentage

--- app/controllers/chart.controllers.client.js
+++ app/controllers/chart.controllers.client.js
@@ -16,10 +16,8 @@ function drawChart(canvas,pollID) {
 
         // individual labels of each data entry
         var labels = result.answers;
-
         // data set
         var data = result.count;
-
         // color of each entry
         var backgroundColor = ["#F48FB1", "#B39DDB", "#81D4FA", "#A5D6A7", "#FFE082", "#BCAAA4", "#B0BEC5"];
 
@@ -37,8 +35,29 @@ function drawChart(canvas,pollID) {
             "options" : {
                 "legend": {
                     "position": "bottom"
+                },
+                "tooltips": {
+                    "position": "nearest",
+                    "callbacks": {
+                        "afterLabel": addPercent2Tooltip //http://www.chartjs.org/docs/latest/configuration/tooltip.html
+                    }
                 }
             }
         });
+
+        // calculate the percent presentation of each entry out of the dataset
+        var totalVoteCount = data.reduce((accumulator, currentItem) => {
+            return accumulator + currentItem;
+        });
+        var percentage = [];
+        data.forEach((voteCount) => {
+            var percentValue = (voteCount/totalVoteCount)*100;
+            percentage.push(percentValue.toFixed(2));
+        });
+
+        // http://www.chartjs.org/docs/latest/configuration/tooltip.html#tooltip-callbacks
+        function addPercent2Tooltip (tooltipItem, data) {
+            return "(" + percentage[tooltipItem.index] + "%)";
+        }
     });
 }
